62d7bdb76dd3bac035d6025e05ed3b5da636c5ae
Kevin J. Sung <kevinjefferysung@gmail.com>
2017-04-01 14:20:31 -0400

    some skeleton code

---
+++ generalized-assignment.py
@@ -0,0 +1,27 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+def getLPSol(p, c, T):
+    """ Get a solution to the LP. There are n jobs and m machines
+    Inputs:
+        p: an mxn matrix
+        c: an mxn matrix
+        T: a number
+    Output: an mxn matrix
+    """
+    return 0
+
+def roundLPSol(x):
+    """ Round an LP solution to an integer solution
+    Inputs:
+        x: an mxn matrix
+    Output: an mxn matrix
+    """
+
+def main():
+    fig = plt.gcf()
+    ax = plt.gca()
+    return 0
+
+if __name__ == '__main__':
+    main()
